9dde35c3821c16068b27d4818d6e6c502cd1a8b8
filipecosta90 <filipecosta.90@gmail.com>
2020-11-26 14:55:35 +0000

    [add] Added install_deps.sh script

---
+++ install_deps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt install -y awscli ansible zip jq
+
+# Ensure terraform is available
+TF_EXE_FILE_NAME=${TF_EXE_FILE_NAME:-$(which terraform)}
+if [[ -z "${TF_EXE_FILE_NAME}" ]]; then
+    echo "terraform not available. It is not specified explicitly and not found in \$PATH"
+    echo "Downloading terraform..."
+    wget https://releases.hashicorp.com/terraform/0.13.5/terraform_0.13.5_linux_amd64.zip
+    unzip terraform_0.13.5_linux_amd64.zip
+    sudo mv terraform /usr/local/bin
+    rm terraform_0.13.5_linux_amd64.zip
+
+fi
+echo "Checking terraform version..."
+${TF_EXE_FILE_NAME} --version
