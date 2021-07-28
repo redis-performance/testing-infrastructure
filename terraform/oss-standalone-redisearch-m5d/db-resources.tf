162f9f8d64ecb4dbcb84d716015ff504d0380cb2
filipecosta90 <filipecosta.90@gmail.com>
2021-07-28 15:29:54 +0100

    [fix] fixed redisearch db-resources.tf ssh key spec

--- terraform/oss-standalone-redisearch-m5d/db-resources.tf
+++ terraform/oss-standalone-redisearch-m5d/db-resources.tf
@@ -1,41 +1,39 @@
-
 resource "aws_instance" "server" {
-  count                  = var.server_instance_count
-  ami                    = var.instance_ami
-  instance_type          = var.server_instance_type
+  count                  = "${var.server_instance_count}"
+  ami                    = "${var.instance_ami}"
+  instance_type          = "${var.server_instance_type}"
   subnet_id              = data.terraform_remote_state.shared_resources.outputs.subnet_public_id
   vpc_security_group_ids = ["${data.terraform_remote_state.shared_resources.outputs.performance_cto_sg_id}"]
-  key_name               = var.key_name
+  key_name               = "${var.key_name}"
   associate_public_ip_address = "true"
   placement_group      = "${data.terraform_remote_state.shared_resources.outputs.perf_cto_pg_name}"
   availability_zone = "us-east-2a"
 
   root_block_device {
-    volume_size           = var.instance_volume_size
-    volume_type           = var.instance_volume_type
-    iops                  = var.instance_volume_iops
-    encrypted             = var.instance_volume_encrypted
+    volume_size           = "${var.instance_volume_size}"
+    volume_type           = "${var.instance_volume_type}"
+    encrypted             = "${var.instance_volume_encrypted}"
     delete_on_termination = true
   }
 
   volume_tags = {
-    Name           = "ebs_block_device-${var.setup_name}-DB-${count.index + 1}"
-    setup          = "${var.setup_name}"
+    Name        = "ebs_block_device-${var.setup_name}-DB-${count.index + 1}"
+    setup        = "${var.setup_name}"
     triggering_env = "${var.triggering_env}"
-    github_actor   = "${var.github_actor}"
-    github_org     = "${var.github_org}"
-    github_repo    = "${var.github_repo}"
-    github_sha     = "${var.github_sha}"
+    github_actor = "${var.github_actor}"
+    github_org = "${var.github_org}"
+    github_repo  = "${var.github_repo}"
+    github_sha   = "${var.github_sha}"
   }
 
   tags = {
-    Name           = "${var.setup_name}-DB-${count.index + 1}"
-    setup          = "${var.setup_name}"
+    Name         = "${var.setup_name}-DB-${count.index + 1}"
+    setup        = "${var.setup_name}"
     triggering_env = "${var.triggering_env}"
-    github_actor   = "${var.github_actor}"
-    github_org     = "${var.github_org}"
-    github_repo    = "${var.github_repo}"
-    github_sha     = "${var.github_sha}"
+    github_actor = "${var.github_actor}"
+    github_org = "${var.github_org}"
+    github_repo  = "${var.github_repo}"
+    github_sha   = "${var.github_sha}"
   }
 
   ################################################################################
@@ -45,11 +43,11 @@ resource "aws_instance" "server" {
     script = "./../scripts/wait_for_instance.sh"
     connection {
       host        = "${self.public_ip}" # The `self` variable is like `this` in many programming languages
-      type        = "ssh"          # in this case, `self` is the resource (the server).
-      user        = var.ssh_user
-      private_key = file(var.private_key)
+      type        = "ssh"               # in this case, `self` is the resource (the server).
+      user        = "${var.ssh_user}"
+      private_key = "${file(var.private_key)}"
       #need to increase timeout to larger then 5m for metal instances
-      timeout = "5m"
+      timeout = "15m"
       agent   = "false"
     }
   }
