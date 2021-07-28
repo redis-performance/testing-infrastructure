6e0c8ce1c0d6d9b49f772fa8724522aa00888faa
filipecosta90 <filipecosta.90@gmail.com>
2021-07-28 15:44:52 +0100

    [fix] Removed requirement of iops property for gp2 disk on redisearch-m5 setup

--- terraform/oss-standalone-redisearch-m5/variables.tf
+++ terraform/oss-standalone-redisearch-m5/variables.tf
@@ -58,6 +58,12 @@ variable "instance_device_name" {
   default     = "/dev/sda1"
 }
 
+
+variable "instance_volume_type" {
+  description = "EC2 instance volume_type"
+  default     = "gp2"
+}
+
 variable "instance_volume_iops" {
   description = "EC2 instance volume_iops"
   default     = "384"
@@ -73,10 +79,6 @@ variable "instance_volume_size" {
   default     = "1024"
 }
 
-variable "instance_volume_type" {
-  description = "EC2 instance volume_type"
-  default     = "io1"
-}
 
 variable "client_instance_volume_size" {
   description = "EC2 instance volume_size"
