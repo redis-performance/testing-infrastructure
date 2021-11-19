c4ce2185378f548ae0ec13f93b4d748d1e43ebe7
filipecosta90 <filipecosta.90@gmail.com>
2021-11-19 12:24:31 +0000

    [wip] reduced verbose level of benchmark outputs

--- terraform/oss-standalone-redistimeseries-m5/variables.tf
+++ terraform/oss-standalone-redistimeseries-m5/variables.tf
@@ -57,7 +57,7 @@ variable "region" {
 # ubuntu-bionic-18.04-amd64-server-20201026
 variable "instance_ami" {
   description = "AMI for aws EC2 instance - us-east-2 Ubuntu 18.04 - perf-cto-base-image-redis6.2.1"
-  default     = "ami-0ed1ccbb18ed2f0ce"
+  default     = "ami-082973cfba046b1d5"
 }
 
 variable "instance_device_name" {
