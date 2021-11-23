b9076570e9987b1cf872b07ca11e514e1ab16fa3
filipe oliveira <filipecosta.90@gmail.com>
2021-11-23 10:29:05 +0000

    Using base image perf-cto-base-image-redis6.2.6perf-daemon-0.5.21 (#33)

--- terraform/oss-standalone-redisbloom-m5/variables.tf
+++ terraform/oss-standalone-redisbloom-m5/variables.tf
@@ -57,7 +57,7 @@ variable "region" {
 # ubuntu-bionic-18.04-amd64-server-20201026
 variable "instance_ami" {
   description = "AMI for aws EC2 instance - us-east-2 Ubuntu 18.04 - perf-cto-base-image-redis6.2.1"
-  default     = "ami-097dda6671eb0e076"  
+  default     = "ami-00d99edf8da73e42b"  
 }
 
 variable "instance_device_name" {
