c1357791eb7b637cdd806846e1922c17423a9de2
filipe oliveira <filipecosta.90@gmail.com>
2021-12-14 12:43:33 +0000

    Moving from m5.8xlarge to c5.4xlarge machines (32VCPUs high-mem to 16VCPUs high-cpu on the client machines) (#34)

--- terraform/oss-standalone-redisbloom-m5/variables.tf
+++ terraform/oss-standalone-redisbloom-m5/variables.tf
@@ -142,14 +142,13 @@ variable "server_instance_cpu_core_count" {
 ################################################################################
 # Specific Client machine variables
 ################################################################################
-# m5.8xlarge 	32 VCPUs 	128 GB MEM
+# c5.4xlarge 	16 VCPUs 
 
 variable "client_instance_type" {
   description = "type for aws EC2 instance"
-  default     = "m5.8xlarge"
+  default     = "c5.4xlarge"
 }
 
 variable "client_instance_count" {
   default = "1"
 }
-
