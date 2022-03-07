794681d4a896953d00565ad3bb44e0c3d80f6dd4
filipe oliveira <filipecosta.90@gmail.com>
2022-03-07 23:07:45 +0000

    Updated base image to perf-cto-base-image-redis6.2.6perf-daemon-0.7.1 (#47)
    * Updated baseimage for search/json to perf-cto-base-image-redis6.2.6perf-daemon-0.7.0
    
    * Updated baseimage for search/json to perf-cto-base-image-redis6.2.6perf-daemon-0.7.1

--- terraform/oss-standalone-redisjson-m5/variables.tf
+++ terraform/oss-standalone-redisjson-m5/variables.tf
@@ -49,8 +49,8 @@ variable "region" {
 }
 
 variable "instance_ami" {
-  description = "AMI for aws EC2 instance - us-east-2 Ubuntu 18.04 - perf-cto-base-image-redis6.2.6perf-daemon-0.7.0"
-  default     = "ami-061a1ef3d036cdfda"
+  description = "AMI for aws EC2 instance - us-east-2 Ubuntu 18.04 - perf-cto-base-image-redis6.2.6perf-daemon-0.7.1"
+  default     = "ami-0f93308ed3bd202b9"
 }
 
 variable "instance_device_name" {
