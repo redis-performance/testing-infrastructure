a8233461ac14ae73f06497e0c2885e40fdf404a4
filipecosta90 <filipecosta.90@gmail.com>
2022-03-15 18:48:11 +0000

    Updated search base image to ami-0a42d18ea4b147b28

--- terraform/oss-standalone-redisjson-m5/variables.tf
+++ terraform/oss-standalone-redisjson-m5/variables.tf
@@ -56,8 +56,8 @@ variable "region" {
 }
 
 variable "instance_ami" {
-  description = "AMI for aws EC2 instance - us-east-2 Ubuntu 18.04 - perf-cto-base-image-redis6.2.6perf-daemon-0.7.10"
-  default     = "ami-0a9349751f375c54c"
+  description = "AMI for aws EC2 instance - us-east-2 Ubuntu 18.04 - perf-cto-base-image-redis6.2.6perf-daemon-0.7.15"
+  default     = "ami-0a42d18ea4b147b28"
 }
 
 variable "instance_device_name" {
