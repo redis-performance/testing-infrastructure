851e28cfea67d31ea0efa10e2c188c2f91d750b2
filipecosta90 <filipecosta.90@gmail.com>
2022-03-25 16:20:08 +0200

    Updated base image

--- terraform/oss-1node-c5.4xlarge/variables.tf
+++ terraform/oss-1node-c5.4xlarge/variables.tf
@@ -64,7 +64,7 @@ variable "region" {
 # ubuntu-bionic-18.04-amd64-server-20201026
 variable "instance_ami" {
   description = "AMI for aws EC2 instance - us-east-2 Ubuntu 18.04 - perf-cto-base-image-redis6.2.1"
-  default     = "ami-01a380731770f044b"
+  default     = "ami-07d00dfddc85e033f"
 }
 
 variable "instance_device_name" {
--- terraform/oss-standalone-redisearch-m5/variables.tf
+++ terraform/oss-standalone-redisearch-m5/variables.tf
@@ -57,7 +57,7 @@ variable "region" {
 
 variable "instance_ami" {
   description = "AMI for aws EC2 instance - us-east-2 Ubuntu 18.04 - perf-cto-base-image-redis6.2.6perf-daemon-0.7.10"
-  default     = "ami-01a380731770f044b"
+  default     = "ami-07d00dfddc85e033f"
 }
 
 variable "instance_device_name" {
