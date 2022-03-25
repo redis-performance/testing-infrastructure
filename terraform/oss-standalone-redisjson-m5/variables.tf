e397a3c06df55a4c47e6d357c740615244dda37e
filipecosta90 <filipecosta.90@gmail.com>
2022-03-25 09:44:20 +0200

    Updated base image to ami-04028d66dbbc030a6

--- terraform/oss-standalone-redisjson-m5/variables.tf
+++ terraform/oss-standalone-redisjson-m5/variables.tf
@@ -57,7 +57,7 @@ variable "region" {
 
 variable "instance_ami" {
   description = "AMI for aws EC2 instance - us-east-2 Ubuntu 18.04 - perf-cto-base-image-redis6.2.6perf-daemon-0.7.15"
-  default     = "ami-0778d227d0419baa3"
+  default     = "ami-04028d66dbbc030a6"
 }
 
 variable "instance_device_name" {
