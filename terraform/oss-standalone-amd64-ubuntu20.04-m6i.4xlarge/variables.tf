687f967a9bb189544c2e45458f7099737d03d051
filipecosta90 <filipecosta.90@gmail.com>
2022-05-12 10:57:17 +0100

    Updated redis-small to m6i.4xlarge setup

---
+++ terraform/oss-standalone-amd64-ubuntu20.04-m6i.4xlarge/variables.tf
@@ -0,0 +1,133 @@
+################################################################################
+# Variables used for deployment tag
+################################################################################
+
+variable "setup_name" {
+  description = "setup name"
+  default     = "oss-standalone-amd64-ubuntu20.04-m6i.4xlarge"
+}
+variable "github_actor" {
+  description = "The name of the person or app that initiated the deployment."
+  default     = "N/A"
+}
+
+variable "github_repo" {
+  description = "	The owner and repository name. For example, octocat/Hello-World."
+  default     = "N/A"
+}
+
+variable "github_sha" {
+  description = "The commit SHA that triggered the deployment."
+  default     = "N/A"
+}
+
+variable "timeout_secs" {
+  description = "The maximum time to wait prior destroying the VM via the watchdog."
+  default     = "3600"
+}
+
+
+
+################################################################################
+# Access keys
+################################################################################
+variable "private_key" {
+  description = "private key"
+  default     = "/tmp/benchmarks.redislabs.pem"
+}
+
+variable "public_key" {
+  description = "public key"
+  default     = "~/.ssh/perf-cto-joint-tasks.pub"
+}
+
+variable "key_name" {
+  description = "key name"
+  default     = "perf-cto-joint-tasks"
+}
+
+variable "region" {
+  default = "us-east-2"
+}
+
+# (Ubuntu 20.04 LTS)
+# ubuntu-bionic-20.04-amd64-server-20220509
+variable "instance_ami" {
+  description = "AMI for aws EC2 instance - us-east-2 Ubuntu 20.04"
+  default     = "ami-072e87c1b25ab0a8b"
+}
+
+variable "instance_device_name" {
+  description = "EC2 instance device name"
+  default     = "/dev/sda1"
+}
+
+variable "redis_module" {
+  description = "redis_module"
+  default     = "N/A"
+}
+
+variable "instance_volume_size" {
+  description = "EC2 instance volume_size"
+  default     = "128"
+}
+
+variable "instance_volume_type" {
+  description = "EC2 instance volume_type"
+  default     = "gp2"
+}
+
+variable "instance_volume_iops" {
+  description = "EC2 instance volume_iops"
+  default     = "384"
+}
+
+variable "instance_volume_encrypted" {
+  description = "EC2 instance instance_volume_encrypted"
+  default     = "false"
+}
+
+variable "instance_root_block_device_encrypted" {
+  description = "EC2 instance instance_root_block_device_encrypted"
+  default     = "false"
+}
+
+# Model	m6i.4xlarge
+variable "instance_type" {
+  description = "type for aws EC2 instance"
+  default     = "m6i.4xlarge"
+}
+
+variable "server_instance_count" {
+  default = "1"
+}
+
+variable "instance_cpu_core_count" {
+  description = "CPU core count for aws EC2 instance"
+  default     = 8
+}
+
+variable "instance_cpu_threads_per_core" {
+  description = "CPU threads per core for aws EC2 instance"
+  default     = 1
+}
+
+variable "instance_cpu_threads_per_core_hyperthreading" {
+  description = "CPU threads per core when hyperthreading is enabled for aws EC2 instance"
+  default     = 2
+}
+
+variable "instance_network_interface_plus_count" {
+  description = "number of additional network interfaces to add to aws EC2 instance"
+  default     = 0
+}
+
+variable "os" {
+  description = "os"
+  default     = "ubuntu20.04"
+}
+
+variable "ssh_user" {
+  description = "ssh_user"
+  default     = "ubuntu"
+}
