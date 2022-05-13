09c91fbce426fb5367353867e134b3d5d42b37d3
filipecosta90 <filipecosta.90@gmail.com>
2022-05-13 12:28:08 +0100

    Updated m6a.8xlarge base image ami-0f28490157d78de6a

--- terraform/oss-standalone-redistimeseries-m6a.8xlarge/variables.tf
+++ terraform/oss-standalone-redistimeseries-m6a.8xlarge/variables.tf
@@ -63,8 +63,8 @@ variable "region" {
 # (Ubuntu 20.04)
 # ubuntu-bionic-20.04-amd64-server
 variable "instance_ami" {
-  description = "AMI for aws EC2 instance - us-east-2 Ubuntu 20.04 - perf-cto-base-image-ubuntu20.04-pd-0.7.40"
-  default     = "ami-02ee3334547ac35ea"
+  description = "AMI for aws EC2 instance - us-east-2 Ubuntu 20.04 - perf-cto-base-image-amd-ubuntu20.04-pd-0.9.2"
+  default     = "ami-0f28490157d78de6a"
 }
 
 variable "instance_device_name" {
