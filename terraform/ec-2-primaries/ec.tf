a3645ecaac7051818fd84f6ff16101d6dfbd486a
filipe oliveira <filipecosta.90@gmail.com>
2023-01-19 12:53:13 +0000

    Added ElasticCache 1,2, and 4 variants (#68)

---
+++ terraform/ec-2-primaries/ec.tf
@@ -0,0 +1,17 @@
+resource "aws_elasticache_replication_group" "ec_2_primaries" {
+  replication_group_id       = "ec-02-primaries"
+  description                = "EC 2 primaries"
+  node_type                  = "cache.m6g.8xlarge"
+  port                       = 6379
+  apply_immediately          = true
+  parameter_group_name       = "default.redis7.cluster.on"
+  automatic_failover_enabled = true
+  multi_az_enabled           = true
+
+  num_node_groups         = 2
+  replicas_per_node_group = 2
+  security_group_ids      = ["${data.terraform_remote_state.shared_resources.outputs.performance_cto_sg_id}"]
+  subnet_group_name         = "ec-multi-az"
+  transit_encryption_enabled = false
+  # auth_token = "performance-at-redis"
+}
