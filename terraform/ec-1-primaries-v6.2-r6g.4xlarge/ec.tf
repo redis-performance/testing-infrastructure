7f22204ce3c44fbd62a7a77f7564f2ba10bad3aa
filipecosta90 <filipecosta.90@gmail.com>
2023-02-13 14:36:49 +0000

    Included r6g... xlarge, 2xlarge, 4xlarge, 8xlarge, and 16xlarge EC setups both for engines 6.x and 7x

---
+++ terraform/ec-1-primaries-v6.2-r6g.4xlarge/ec.tf
@@ -0,0 +1,13 @@
+resource "aws_elasticache_cluster" "ec" {
+  cluster_id           = "ec-1-primaries-v6x-r6g-4xlarge"
+  node_type            = "cache.r6g.4xlarge"
+  port                 = 6379
+  apply_immediately    = true
+  parameter_group_name = "default.redis6.x"
+  engine               = "redis"
+  engine_version       = "6.x"
+  num_cache_nodes      = 1
+  security_group_ids   = ["${data.terraform_remote_state.shared_resources.outputs.performance_cto_sg_id}"]
+  subnet_group_name    = "ec-subnet"
+  availability_zone    = "us-east-2a"
+}
