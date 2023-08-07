3f9cf85f3328fae7119c7f551ff6969e60e30ee9
filipe oliveira <filipecosta.90@gmail.com>
2023-08-07 18:24:08 +0300

    Fixed redisgraph-r5 setup missing client  (#83)
    * Updated base image to use memtier v2
    
    * Fixed redisgraph-r5 setup missing client

--- terraform/oss-standalone-redisgraph-r5/output.tf
+++ terraform/oss-standalone-redisgraph-r5/output.tf
@@ -5,3 +5,11 @@ output "server_public_ip" {
 output "server_private_ip" {
   value = ["${aws_instance.server[0].private_ip}"]
 }
+
+output "client_public_ip" {
+  value = ["${aws_instance.client[0].public_ip}"]
+}
+
+output "client_private_ip" {
+  value = ["${aws_instance.client[0].private_ip}"]
+}
