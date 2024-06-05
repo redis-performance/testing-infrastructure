ba7489a8e9ed89505d09d4c8898e5ea9eb2020f0
filipecosta90 <filipecosta.90@gmail.com>
2024-06-05 17:30:05 +0100

    Added CE Redis easy deployment

--- terraform/scripts/install_ce_redis.sh
+++ terraform/scripts/install_ce_redis.sh
@@ -7,5 +7,5 @@ curl -fsSL https://packages.redis.io/gpg | sudo gpg --yes --dearmor -o /usr/shar
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 
 sudo apt-get update
-sudo apt-get install memtier-benchmark -y
-memtier_benchmark --version
+sudo apt-get install redis -y
+redis-server --version
