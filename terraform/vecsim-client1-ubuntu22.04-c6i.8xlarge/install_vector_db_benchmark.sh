b3f738e6925b6fe575f173ebf021f2e79b5b9abc
filipecosta90 <filipecosta.90@gmail.com>
2024-07-08 17:43:26 +0100

    Added vecsim benchmark client setup with ubuntu22.04 os

---
+++ terraform/vecsim-client1-ubuntu22.04-c6i.8xlarge/install_vector_db_benchmark.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+sudo DEBIAN_FRONTEND=noninteractive apt update -y
+sudo DEBIAN_FRONTEND=noninteractive apt install python3-pip -y
+sudo DEBIAN_FRONTEND=noninteractive pip3 install --upgrade pip 
+sudo pip3 install ml-dtypes==0.4.0 stopit numpy redis typer dataclasses tqdm h5py elasticsearch==8.12.1 pymilvus backoff opensearch-py qdrant-client psycopg[binary] weaviate-client==4.5.4 pgvector
+sudo git clone https://github.com/redis-performance/vector-db-benchmark --branch update.redisearch
