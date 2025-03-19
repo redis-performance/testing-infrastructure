c5ead230dc5ccb53c97c8e34ebc8b2234a6650af
filipecosta90 <filipecosta.90@gmail.com>
2025-03-19 11:35:57 +0000

    Added 24.04 arm64 base image

---
+++ terraform/perf-base-image-ubuntu24.04-aarch64-m7g.8xlarge/common.tf
@@ -0,0 +1,13 @@
+
+################################################################################
+# This is the bucket holding this specific setup tfstate
+################################################################################
+terraform {
+  backend "s3" {
+    bucket = "performance-cto-group"
+    region = "us-east-1"
+    key = "benchmarks/infrastructure/perf-base-image-ubuntu24.04-aarch64-m7g.8xlarge.tfstate"
+
+  }
+}
+
