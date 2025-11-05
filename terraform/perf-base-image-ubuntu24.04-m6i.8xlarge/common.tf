be748ea473f77b63ae1e51172d79861175a71a9a
fcostaoliveira <filipe@redis.com>
2025-11-05 13:49:08 +0000

    Updated base image of search to redis 8.4: ami-09f0d91e70e7a2515

--- terraform/perf-base-image-ubuntu24.04-m6i.8xlarge/common.tf
+++ terraform/perf-base-image-ubuntu24.04-m6i.8xlarge/common.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     bucket = "performance-cto-group"
     region = "us-east-1"
-    key = "benchmarks/infrastructure/perf-base-image-ubuntu24.04-m6i.8xlarge.tfstate"
+    key = "benchmarks/infrastructure/perf-base-image-ubuntu24.04-m6i.8xlarge-redis-8.4.tfstate"
 
   }
 }
