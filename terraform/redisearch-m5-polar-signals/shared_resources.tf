81151569f443e6ed7f3988ed6468feb4266a35a2
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2026-02-09 10:36:04 +0100

    create redisearch-m5 polar signals variant

---
+++ terraform/redisearch-m5-polar-signals/shared_resources.tf
@@ -0,0 +1,27 @@
+# provider
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.94.1"
+    }
+  }
+}
+
+################################################################################
+# This is the shared resources bucket key -- you will need it across environments like security rules,etc...
+# !! do not change this !!
+################################################################################
+data "terraform_remote_state" "shared_resources" {
+  backend = "s3"
+  config = {
+    bucket = "performance-cto-group"
+    key    = "benchmarks/infrastructure/shared_resources.tfstate"
+    region = "us-east-1"
+  }
+}
+
---
+++ terraform/redisearch-m5-polar-signals/variables.tf
@@ -0,0 +1,188 @@
+################################################################################
+# Variables used for deployment tag
+################################################################################
+
+variable "setup_name" {
+  description = "setup name"
+  default     = "oss-redisearch-m5-polar-signals"
+}
+variable "github_actor" {
+  description = "The name of the person or app that initiated the deployment."
+  default     = "N/A"
+}
+
+variable "github_repo" {
+  description = "	The owner and repository name. For example, testing-infrastructure."
+  default     = "N/A"
+}
+
+variable "triggering_env" {
+  description = "	The triggering environment. For example circleci."
+  default     = "N/A"
+}
+
+variable "environment" {
+  description = "	The cost tag."
+  default     = "RediSearch"
+}
+
+variable "github_org" {
+  description = "	The owner name. For example, RedisModules."
+  default     = "N/A"
+}
+
+variable "github_sha" {
+  description = "The commit SHA that triggered the deployment."
+  default     = "N/A"
+}
+
+variable "timeout_secs" {
+  description = "The maximum time to wait prior destroying the VM via the watchdog."
+  default     = "3600"
+}
+
+
+
+################################################################################
+# Access keys
+################################################################################
+variable "private_key" {
+  description = "private key"
+  default     = "/tmp/benchmarks.redislabs.pem"
+}
+
+variable "key_name" {
+  description = "key name"
+  default     = "perf-cto-us-east-2"
+}
+
+variable "region" {
+  default = "us-east-2"
+}
+
+# (Ubuntu 24.04, memtier 2.1.4 and redis 8.4 from Nov 5th 2025)
+# https://us-east-2.console.aws.amazon.com/ec2/home?region=us-east-2#ImageDetails:imageId=ami-09f0d91e70e7a2515
+variable "instance_ami" {
+  description = "AMI for aws EC2 instance - us-east-2 Ubuntu 24.04 - perf-base-image-ubuntu24.04-m6i.8xlarge-20250325-0937"
+  default     = "ami-09f0d91e70e7a2515"
+}
+
+variable "instance_device_name" {
+  description = "EC2 instance device name"
+  default     = "/dev/sda1"
+}
+
+
+variable "instance_volume_type" {
+  description = "EC2 instance volume_type"
+  default     = "gp3"
+}
+
+variable "instance_volume_iops" {
+  description = "EC2 instance volume_iops"
+  default     = "100"
+}
+
+variable "redis_module" {
+  description = "redis_module"
+  default     = "N/A"
+}
+
+variable "instance_volume_size" {
+  description = "EC2 instance volume_size"
+  default     = "1024"
+}
+
+
+variable "client_instance_volume_size" {
+  description = "EC2 instance volume_size"
+  default     = "256"
+}
+
+variable "client_instance_volume_type" {
+  description = "EC2 instance volume_type"
+  default     = "gp3"
+}
+
+
+variable "instance_volume_encrypted" {
+  description = "EC2 instance instance_volume_encrypted"
+  default     = "false"
+}
+
+variable "instance_root_block_device_encrypted" {
+  description = "EC2 instance instance_root_block_device_encrypted"
+  default     = "false"
+}
+
+variable "instance_cpu_threads_per_core" {
+  description = "CPU threads per core for aws EC2 instance"
+  default     = 1
+}
+
+variable "instance_cpu_threads_per_core_hyperthreading" {
+  description = "CPU threads per core when hyperthreading is enabled for aws EC2 instance"
+  default     = 2
+}
+
+variable "instance_network_interface_plus_count" {
+  description = "number of additional network interfaces to add to aws EC2 instance"
+  default     = 0
+}
+
+variable "os" {
+  description = "os"
+  default     = "ubuntu20.04"
+}
+
+variable "ssh_user" {
+  description = "ssh_user"
+  default     = "ubuntu"
+}
+
+################################################################################
+# Specific DB machine variables
+################################################################################
+# m5.8xlarge 	32 VCPUs 	128 GB MEM
+variable "server_instance_type" {
+  description = "type for aws EC2 instance"
+  default     = "m6i.8xlarge"
+}
+
+
+variable "server_instance_count" {
+  default = "1"
+}
+
+variable "server_instance_cpu_core_count" {
+  description = "CPU core count for aws EC2 instance"
+  default     = 16
+}
+
+variable "server_instance_cpu_threads_per_core" {
+  description = "CPU threads per CORE"
+  default     = 1
+}
+
+################################################################################
+# Specific Client machine variables
+################################################################################
+# c5.4xlarge 	16 VCPUs
+
+variable "client_instance_type" {
+  description = "type for aws EC2 instance"
+  default     = "m6i.4xlarge"
+}
+
+variable "client_instance_count" {
+  default = "1"
+}
+
+################################################################################
+# Polar Signals / Parca agent configuration
+################################################################################
+variable "parca_agent_token" {
+  description = "Bearer token for Polar Signals remote store"
+  type        = string
+  sensitive   = true
+}
